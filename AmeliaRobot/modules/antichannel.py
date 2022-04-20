7881693f8d7bfb5f8f2fd8abb0398b429761c0b8
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-20 15:43:27 +0100

    Update antichannel.py

--- AmeliaRobot/modules/antichannel.py
+++ AmeliaRobot/modules/antichannel.py
@@ -76,7 +76,7 @@ If enabled, the message from the channel which the user sends will be banned.
 
 ANTICHANNEL_HANDLER = CommandHandler("antichannel", set_antichannel)
 ELIMINATE_CHANNEL_HANDLER = MessageHandler(
-    Filters.chat_type.groups, eliminate_channel
+    Filters.chat_type.groups, eliminate_channel, run_async=True
 )
 
 dispatcher.add_handler(ANTICHANNEL_HANDLER, SET_CH_GROUP)
