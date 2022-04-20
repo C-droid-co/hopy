0931723a4f84d96cb784f4e1cba6175440b87070
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-20 07:38:01 +0100

    Update handlers.py

--- AmeliaRobot/modules/helper_funcs/handlers.py
+++ AmeliaRobot/modules/helper_funcs/handlers.py
@@ -1,6 +1,5 @@
 import AmeliaRobot.modules.sql.blacklistusers_sql as sql
-from AmeliaRobot import ALLOW_EXCL
-from AmeliaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+from AmeliaRobot import PREFIX, DEV_USERS, SUPPORT_USERS
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
@@ -12,21 +11,10 @@ from pyrate_limiter import (
     MemoryListBucket,
 )
 
-if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "!")
-else:
-    CMD_STARTERS = ("/",)
-
 
 class AntiSpam:
     def __init__(self):
-        self.whitelist = (
-            (DEV_USERS or [])
-            + (DRAGONS or [])
-            + (WOLVES or [])
-            + (DEMONS or [])
-            + (TIGERS or [])
-        )
+        self.whitelist = DEV_USERS | SUPPORT_USERS
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
@@ -83,7 +71,7 @@ class CustomCommandHandler(CommandHandler):
             if message.text and len(message.text) > 1:
                 fst_word = message.text.split(None, 1)[0]
                 if len(fst_word) > 1 and any(
-                    fst_word.startswith(start) for start in CMD_STARTERS
+                    fst_word.startswith(start) for start in PREFIX
                 ):
 
                     args = message.text.split()[1:]
@@ -136,4 +124,8 @@ class CustomMessageHandler(MessageHandler):
 
         def check_update(self, update):
             if isinstance(update, Update) and update.effective_message:
-                return self.filters(update)
+                if self.filters(update):
+                    if SpamChecker.check_user(user_id):
+                        return None
+                    return True
+                return False
