65f6a9dc31cea3abd28b6688d5299f73abb87abe
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:02:45 +0100

    Create filters.py

---
+++ AmeliaRobot/modules/Admin/filters.py
@@ -0,0 +1,4 @@
+__mod_name__ = "Filters"
+__help__ = """/filters To Get All The Filters In The Chat.
+/filter [FILTER_NAME] To Save A Filter (Can be a sticker or text).
+/stop [FILTER_NAME] To Stop A Filter.
