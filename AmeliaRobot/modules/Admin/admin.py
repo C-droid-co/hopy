c4112035610dc6c5ee35bdd499a9914c42a207fe
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 22:38:53 +0100

    Create admin.py

---
+++ AmeliaRobot/modules/Admin/admin.py
@@ -0,0 +1,25 @@
+__mod_name__ = "Admin"
+__help__ = """/ban - Ban A User
+/dban - Delete the replied message banning its sender
+/tban - Ban A User For Specific Time
+/unban - Unban A User
+/warn - Warn A User
+/dwarn - Delete the replied message warning its sender
+/rmwarns - Remove All Warning of A User
+/warns - Show Warning Of A User
+/kick - Kick A User
+/dkick - Delete the replied message kicking its sender
+/purge - Purge Messages
+/purge [n] - Purge "n" number of messages from replied message
+/del - Delete Replied Message
+/promote - Promote A Member
+/fullpromote - Promote A Member With All Rights
+/demote - Demote A Member
+/pin - Pin A Message
+/mute - Mute A User
+/tmute - Mute A User For Specific Time
+/unmute - Unmute A User
+/ban_ghosts - Ban Deleted Accounts
+/report | @admins | @admin - Report A Message To Admins.
+/admincache - Reload admin list"""
+
