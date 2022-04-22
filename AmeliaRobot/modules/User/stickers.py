409f5ff7f3e18182faa6c450f9461d3f6154c9e6
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:31:00 +0100

    Create stickers.py

---
+++ AmeliaRobot/modules/User/stickers.py
@@ -0,0 +1,8 @@
+__mod_name__ = "Stickers"
+__help__ = """
+/sticker_id
+    To get FileID of a Sticker.
+/get_sticker
+    To get sticker as a photo and document.
+/kang
+    To kang a Sticker or an Image."""
