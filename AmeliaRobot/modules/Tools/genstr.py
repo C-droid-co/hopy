950657e2beb633cfccad943674a4d6c7c71ec6cc
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:05:57 +0100

    Create genstr.py

---
+++ AmeliaRobot/modules/Tools/genstr.py
@@ -0,0 +1,4 @@
+__mod_name__ = "GenStr"
+__help__ = """
+This Module Can Be Used To Generate Session String For A Userbot.
+Send **/genstr** Command To The Bot In Private And Follow Instructions."""
