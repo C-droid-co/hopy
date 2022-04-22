2efd8577a587ebaedee84840a8b30ce90b952606
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:14:39 +0100

    Create locks.py

---
+++ AmeliaRobot/modules/Admin/locks.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Locks"
+__help__ = """
+Commands: /lock | /unlock | /locks [No Parameters Required]
+Parameters:
+    messages | stickers | gifs | media | games | polls
+    inline  | url | group_info | user_add | pin
+You can only pass the "all" parameter with /lock, not with /unlock
+Example:
+    /lock all
+"""
