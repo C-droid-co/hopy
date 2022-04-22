97ea9f8521aaa38bcc6838c9d409152135b53746
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:20:01 +0100

    Create notes.py

---
+++ AmeliaRobot/modules/Admin/notes.py
@@ -0,0 +1,7 @@
+__mod_name__ = "Notes"
+__help__ = """/notes To Get All The Notes In The Chat.
+/save [NOTE_NAME] To Save A Note (Can be a sticker or text).
+#NOTE_NAME To Get A Note.
+/delete [NOTE_NAME] To Delete A Note.
+Checkout /markdownhelp to know more about formattings and other syntax.
+"""
