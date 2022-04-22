81d73756f7470b2c08f6933764502b0a3f74eac3
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 22:40:54 +0100

    Create admin_misc.py

---
+++ AmeliaRobot/modules/Admin/admin_misc.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Admin Misc"
+__help__ = """
+/set_title - Change The Name Of A Group/Channel.
+/set_photo - Change The PFP Of A Group/Channel.
+/set_user_title - Change The Administrator Title Of An Admin.
+"""
