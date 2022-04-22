a5b4f41c1599f77c3077c0fb578abc17c4b7a348
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:17:50 +0100

    Create music.py

---
+++ AmeliaRobot/modules/Tools/music.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Music"
+__help__ = """
+/ytmusic [link] To Download Music From Various Websites Including Youtube. [SUDOERS]
+/saavn [query] To Download Music From Saavn.
+/lyrics [query] To Get Lyrics Of A Song.
+"""
