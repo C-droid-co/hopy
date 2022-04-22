38b82125f2f27743286c53fb4634aa4cbc5d38ff
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:16:48 +0100

    Create misc.py

---
+++ AmeliaRobot/modules/Tools/misc.py
@@ -0,0 +1,41 @@
+__mod_name__ = "Misc"
+__help__ = """
+/asq
+    Ask a question
+/commit
+    Generate Funny Commit Messages
+/runs
+    Idk Test Yourself
+/id
+    Get Chat_ID or User_ID
+/random [Length]
+    Generate Random Complex Passwords
+/cheat [Language] [Query]
+    Get Programming Related Help
+/tr [LANGUAGE_CODE]
+    Translate A Message
+    Ex: /tr en
+/json [URL]
+    Get parsed JSON response from a rest API.
+/arq
+    Statistics Of ARQ API.
+/webss | .webss [URL] [FULL_SIZE?, use (y|yes|true) to get full size image. (optional)]
+    Take A Screenshot Of A Webpage
+/reverse
+    Reverse search an image.
+/carbon
+    Make Carbon from code.
+/tts
+    Convert Text To Speech.
+/autocorrect [Reply to a message]
+    Autocorrects the text in replied message.
+/pdf [Reply to an image (as document) or a group of images.]
+    Convert images to PDF, helpful for online classes.
+/markdownhelp
+    Sends mark down and formatting help.
+/backup
+    Backup database
+/ping
+    Check ping of all 5 DCs.
+   
+"""
