4b3ae18bb9178d192c3777c8364eaddb5148ff57
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-22 23:13:17 +0100

    Create karma.py

---
+++ AmeliaRobot/modules/User/karma.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Karma"
+__help__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+[DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
+/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
+Reply to a message with /karma to check a user's karma
+Send /karma without replying to any message to check karma list of top 10 users"""
