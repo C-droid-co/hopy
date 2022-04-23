09e2f58235fcbfcc3cb2661889779dd63e45a90b
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-23 10:28:29 +0100

    Update chatstatus.py

--- AmeliaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ AmeliaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -36,7 +36,7 @@ async def is_user_admin(user_id: int, chat_id):
     async for user in telethn.iter_participants(
         chat_id, filter=ChannelParticipantsAdmins
     ):
-        if user_id == user.id or user_id in DRAGONS:
+        if user_id == user.id or user_id in INSPECTOR:
             status = True
             break
     return status
